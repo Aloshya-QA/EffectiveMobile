b7e93eaee5340dcacbe6e4ea2da3d7c24afc9edb
Aloshkya <alexis.belyakoff@yandex.ru>
2025-08-18 11:20:32 +0300

    add screenshot on test failures for Allure reports

--- tests/test_landing_page.py
+++ tests/test_landing_page.py
@@ -6,9 +6,13 @@ class TestLandingPage:
 
     BUTTONS = ["about_us", "more_info", "cases", "reviews", "contacts"]
 
+    @pytest.fixture
+    def landing_page_opened(landing_page):
+        landing_page.open().is_opened()
+        return landing_page
+
     @allure.title("Checking click buttons")
     @pytest.mark.parametrize("button", BUTTONS)
-    def test_landing_buttons(self, landing_page, button):
-        landing_page.open().is_opened().click_button(button)
-        check.is_true(landing_page.button_is_clicked(button),
-                      f"Button '{button}' click failed")
+    def test_landing_buttons(self, button):
+        landing_page_opened.click_button(button)
+        check.is_true(landing_page_opened.button_is_clicked(button), f"Button '{button}' click failed")
